d4789c2943375fc7da6507a59faa6a7a2d2a9e36
Christiaan Benedictus <chris@combat.nl>
2025-03-03 15:10:16 +0100

    More typo fixes

--- lua/h4rdc0m/plugins/copilot.lua
+++ lua/h4rdc0m/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   "github/copilot.vim",
   lazy = false,
   config = function()
-    vim.g.copilot_no_tab_map = true,
-    vim.g.copilor_assume_mapped = true,
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilor_assume_mapped = true
   end,
 }
